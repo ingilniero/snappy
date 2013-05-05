c422ebf8510437f8d15503050640ed0f50987e7c
Gilberto Villa <ingilniero@gmail.com>
2013-05-05 17:55:26 -0500

    Users can ask questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,21 @@
 class QuestionsController < ApplicationController
   def index
+    @question = Question.new
   end
+
+  def create
+    @question = current_user.questions.build question_params
+
+    if @question.save
+      flash[:success] = 'Your question has been posted!'
+      redirect_to root_url
+    else
+      render 'index'
+    end
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Snappy::Application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
+  resources :questions, except: [:new]
 
   get '/register', to: 'users#new'
   get '/login', to: 'sessions#new'
