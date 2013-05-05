b970c067d2b052d8ad2eed111d5d64fab1b57be8
Gilberto Villa <ingilniero@gmail.com>
2013-05-05 17:16:16 -0500

    Add user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: sessions_params[:username])
+
+    if user and user.authenticate(sessions_params[:password])
+      login user
+      flash[:success] = 'You are now logged in!'
+      redirect_to root_url
+    else
+      flash.now[:error] = 'Your username/password was incorrect'
+      render 'new'
+    end
+  end
+
+  private
+    def sessions_params
+      params.require(:session).permit(:username, :password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Snappy::Application.routes.draw do
-  get "sessions/new"
   root to: 'questions#index'
 
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
