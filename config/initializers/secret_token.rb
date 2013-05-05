7730ee3631bc790a0f047841a0824df1056d74fd
Gilberto Villa <ingilniero@gmail.com>
2013-05-05 15:28:58 -0500

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snappy::Application.config.secret_key_base = '8125760f861927aa0382d922dc742d7b917de7979a6d5826d530945d06d8651674b5672b52e9a215b3a859e134353a323f9af5b20f001caa0cf40970e4cd13a3'
