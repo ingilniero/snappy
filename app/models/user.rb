21bcb1f2d3c45a9acdcc502677e28eaaadf85b5b
Gilberto Villa <ingilniero@gmail.com>
2013-05-05 17:37:43 -0500

    Add Question model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_many :questions
+
   validates :username, presence: true, uniqueness: { case_sensitive: false },
                        length: { in: 4..12 },
                        format: { with: /[a-z][a-z0-9]*/, message: 'can only contain lowecase letters and numbers' }
