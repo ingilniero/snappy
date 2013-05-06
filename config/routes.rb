751cabd7b7fb3cc40970195b74e321402917208d
Gilberto Villa <ingilniero@gmail.com>
2013-05-06 01:10:03 -0500

    Users can answer questions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Snappy::Application.routes.draw do
 
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create]
-  resources :questions, except: [:new]
+  resources :questions, except: [:new] do
+    resources :answers, only: [:create]
+  end
 
   get '/register', to: 'users#new'
   get '/login', to: 'sessions#new'
